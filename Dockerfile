2b3826122e645c5b9e7dbe4cc71413df33844135
Sangheon Han <han@sangheon.com>
2024-02-26 15:27:40 +0900

    Update Dockerfile and entrypoint.sh to configure app user and permissions

--- Dockerfile
+++ Dockerfile
@@ -17,13 +17,21 @@ rsync exuberant-ctags black python3-venv gpg sudo gosu && \
 echo "Asia/Seoul" > /etc/timezone; \
 ln -fs /usr/share/zoneinfo/`cat /etc/timezone` /etc/localtime && \
 git config --global pull.rebase false && \
-cd && git clone https://github.com/sangheonhan/dotfiles.git && \
-cd ~/dotfiles/ && ./bootstrap.sh -f && \
 locale-gen ko_KR.UTF-8 && \
 update-locale LANG=ko_KR.UTF-8 && \
 dpkg-reconfigure --frontend noninteractive locales && \
 echo "export LC_MESSAGES=POSIX" >> ~/.extra; \
 chsh -s /bin/zsh root && \
+usermod -l app ubuntu && \
+groupmod -n app ubuntu && \
+usermod -d /home/app -m app && \
+chown -R app:app /home/app && \
+passwd -d app && \
+usermod -c "Application" app && \
+chsh -s /bin/zsh app && \
+echo "app ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/app && \
+chmod 0440 /etc/sudoers.d/app && \
+sudo -u app bash -c "cd /home/app/ && git clone https://github.com/sangheonhan/dotfiles.git && cd ./dotfiles && ./bootstrap.sh -f && echo 'export LC_MESSAGES=POSIX' >> ~/.extra" && \
 apt clean autoclean -y && \
 apt autoremove -y && \ 
 chmod +x /usr/local/bin/entrypoint.sh && \
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,8 @@
 #! /bin/bash
 
-UBUNTU_USER=ubuntu
 UBUNTU_USER_UID=1000
 UBUNTU_USER_GID=1000
-USERNAME=app
-USER_HOME=/home/$USERNAME
-
-if getent passwd $USERNAME > /dev/null 2>&1; then
-    echo "User $USERNAME already exists"
-    exit 0
-fi
+USER_HOME=/home/app
 
 if [ -z "$HOST_UID" ] || [ -z "$HOST_GID" ]; then
     echo "HOST_UID and HOST_GID must be set"
@@ -25,30 +18,11 @@ if [ $HOST_GID -lt $UBUNTU_USER_GID ]; then
     HOST_GID=$UBUNTU_USER_GID
 fi
 
-echo "Changing $UBUNTU_USER user to $USERNAME ($HOST_UID:$HOST_GID)"
-usermod -l $USERNAME $UBUNTU_USER
-groupmod -n $USERNAME $UBUNTU_USER
-usermod -d $USER_HOME -m $USERNAME
-passwd -d $USERNAME
-chsh -s /bin/zsh $USERNAME
-usermod -u $HOST_UID $USERNAME
-groupmod -g $HOST_GID $USERNAME
-chown -R $USERNAME:$USERNAME $USER_HOME
-
-if [ ! -f /etc/sudoers.d/$USERNAME ]; then
-    echo "$USERNAME ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/$USERNAME
-    chmod 0440 /etc/sudoers.d/$USERNAME
-fi
-
-if [ ! -d $USER_HOME/.vim ] || [ ! -d $USER_HOME/.oh-my-zsh ] || [ ! -d $USER_HOME/.fzf ]; then
-    TEMP_DIR=$(sudo -u $USERNAME mktemp -d)
-    sudo -u $USERNAME bash -c "cd $TEMP_DIR && git clone https://github.com/sangheonhan/dotfiles.git && cd $TEMP_DIR/dotfiles && ./bootstrap.sh -f && rm -rf $TEMP_DIR"
-fi
+echo "Changing UID and GID of app to $HOST_UID and $HOST_GID"
+usermod -u $HOST_UID app
+groupmod -g $HOST_GID app
 
 chown $HOST_UID:$HOST_GID /app/
 chmod 0755 /app/
 
-echo "Starting with Username: $USERNAME, UID: $HOST_UID, GID: $HOST_GID"
-echo "Now you can use this container."
-
-exec gosu $USERNAME "$@"
+exec gosu app "$@"
